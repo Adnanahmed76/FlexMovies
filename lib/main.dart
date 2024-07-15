34cf626e72f3a668eb3df3e2fdc363a404993e5f
Adnan Ahmed <adnanahmed504423@gmail.com>
2024-07-15 23:53:30 +0530

    final

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flexmovies/widgets/toprated.dart';
 import 'package:flexmovies/widgets/trending.dart';
 import 'package:flexmovies/widgets/tv.dart';
 import 'package:flutter/material.dart';
-import 'package:tmdb_api/tmdb_api.dart';
+
 
 void main()=>runApp(new MyApp());
 
