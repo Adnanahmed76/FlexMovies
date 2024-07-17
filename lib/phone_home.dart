5ac9b1289ad5a27f6da210cd30994d1319cf2bd0
Adnan Ahmed <adnanahmed504423@gmail.com>
2024-07-17 23:17:29 +0530

    final

--- lib/phone_home.dart
+++ lib/phone_home.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 class PhoneHome extends StatefulWidget {
   const PhoneHome({super.key});
 
@@ -10,10 +11,24 @@ class PhoneHome extends StatefulWidget {
 class _PhoneHomeState extends State<PhoneHome> {
   TextEditingController phonenumber=TextEditingController();
   sendcode() async{
-    await FirebaseAuth.instance.verifyPhoneNumber(
+ try {
+     await FirebaseAuth.instance.verifyPhoneNumber(
       phoneNumber: '+91' + phonenumber.text,
       
-      verificationCompleted:  , verificationFailed: verificationFailed, codeSent: codeSent, codeAutoRetrievalTimeout: codeAutoRetrievalTimeout)
+      verificationCompleted:  (PhoneAuthCredential credential){}, verificationFailed: (FirebaseAuthException e){
+        Get.snackbar("Error", e.code);
+      }, codeSent: (String vid,int? token){
+        Get.to(OtpPage(vid: vid));
+      }, codeAutoRetrievalTimeout: (vid){
+
+      });
+ }
+ on FirebaseAuthException catch(e){
+  Get.snackbar("Error Occured", e.code);
+ }
+  catch (e) {
+   Get.snackbar("Error", e.toString());
+ }
   }
   @override
   Widget build(BuildContext context) {
@@ -21,8 +36,58 @@ class _PhoneHomeState extends State<PhoneHome> {
       body: ListView(
         children: [
           Image.asset("assets/auth.jpg"),
-          Center(child:Text("Phone Number",style: TextStyle(fontSize: 30,fontWeight: FontWeight.w400),) ,)
+          Center(child:Text("Phone Number",style: TextStyle(fontSize: 30,fontWeight: FontWeight.w400),) ,),
+          Padding(padding: EdgeInsets.symmetric(horizontal: 25,vertical: 6),
+          child: Text("We Will send you an one time password on this mobile number"),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          
         ],
+      ),
+    );
+  }
+  Widget phonetext(){
+    return Padding(padding: EdgeInsets.symmetric(horizontal: 50),
+    child: TextField(
+      controller: phonenumber,
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        prefix: Text("+91 "),
+        prefixIcon: Icon(Icons.phone),
+        labelText: "Enter Phone Number",
+        hintStyle: TextStyle(color: Colors.grey),
+        labelStyle: TextStyle(color: Colors.grey),
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.blue),
+        ),
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.blue
+          )
+        )
+      ),
+    ),
+    );
+  }
+  Widget button(){
+    return Center(
+      child: ElevatedButton(onPressed: (){
+
+      },style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.blueAccent,
+        padding: EdgeInsets.all(16)
+      ),
+      child: Padding(padding: EdgeInsets.symmetric(horizontal: 90),
+      child: Text(
+        "Resend Otp",
+        style: TextStyle(
+
+        ),
+      ),
+      ),
+      
       ),
     );
   }
