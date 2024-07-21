beb29e694959008fd7142a9879f36aa21bf4428f
Adnan Ahmed <adnanahmed504423@gmail.com>
2024-07-21 21:42:41 +0530

    final

--- lib/description.dart
+++ lib/description.dart
@@ -31,7 +31,7 @@ class Description extends StatelessWidget {
                     child: Container(
                       height: 250,
                       width: MediaQuery.of(context).size.width,
-                      child: bannerurl !=  
+                      child: bannerurl != null
                         ? Image.network(bannerurl!, fit: BoxFit.cover) 
                         : Container(color: Colors.grey),
                     ),
